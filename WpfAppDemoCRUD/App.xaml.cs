ad934b39cb94a466121f411dfaf6c9cbe03df2be
Arayik Gevorgyan <72850471+ArayikGevorgyan@users.noreply.github.com>
2024-04-07 13:20:54 +0400

    Update App.xaml.cs
    comments added

--- WpfAppDemoCRUD/App.xaml.cs
+++ WpfAppDemoCRUD/App.xaml.cs
@@ -16,19 +16,20 @@ namespace WpfAppDemoCRUD
         private ServiceProvider serviceProvider;
 
         public App()
-        {
+        {    // This is the place where we can register our DBcontext. 
             ServiceCollection services = new ServiceCollection();
             services.AddDbContext<ProductDbContext>(option =>
             {
                 option.UseSqlite("Data Source = Product.db");
             });
-
+            
+            // Register main window as singleton
             services.AddSingleton<MainWindow>();
             serviceProvider = services.BuildServiceProvider();
         }
 
         private void OnStartup(object s, StartupEventArgs e)
-        {
+        {    // Get main window instance from service provider and show it
             var mainWindow = serviceProvider.GetService<MainWindow>();
             mainWindow.Show();
         }
